9ecc9d3c7d4566af172403bf5f6c422d51e97df5
Ansar1503 <muhammedansarma1503@gmail.com>
2026-06-25 11:50:06 +0530

    delete bookings

--- src/components/DashboardTable.tsx
+++ src/components/DashboardTable.tsx
@@ -1,12 +1,24 @@
 import { StatusBadge } from "@/components/StatusBadge"
 import type { Booking } from "@/types/types";
 import { Trash2 } from "lucide-react"
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 interface DashboardTableProps {
     bookings: Booking[];
 }
 
 function DashboardTable({bookings}: DashboardTableProps) {
+    const [bookingList,setBookingList] = useState<Booking[]>(bookings)
+    const handleDelete = (id:number)=>{
+        if(!id){
+            toast.error('invalid user! Id not found')
+        }else{
+            const newBookingList = bookingList.filter((booking)=>booking.id !== id)
+            setBookingList(newBookingList)
+            toast.success('booking deleted successfully')
+        }
+    }
     return (
         <div className="mt-10 mx-8">
             <table className="w-full text-left border-collapse">
@@ -21,14 +33,14 @@ function DashboardTable({bookings}: DashboardTableProps) {
                     </tr>
                 </thead>
                 <tbody className="bg-neutral-200 text-left">
-                   {bookings.map((booking) => (
+                   {bookingList.map((booking) => (
                     <tr key={booking.id}>
                         <td className="p-2">{booking.id}</td>
                         <td className="p-2">{booking.clientName}</td>
                         <td className="p-2">{booking.sessionType}</td>
                         <td className="p-2">{booking.date}</td>
                         <td className="p-2"><StatusBadge status={booking.status} /></td>
-                        <td className="text-right pr-4"><button className="p-2 cursor-pointer text-red-600 hover:text-red-400 ">
+                        <td className="text-right pr-4"><button onClick={()=>{handleDelete(booking.id)}} className="p-2 cursor-pointer text-red-600 hover:text-red-400 ">
                                 <Trash2 size={18} />
                             </button></td>
                     </tr>
