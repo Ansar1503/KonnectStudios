5c7a0f38e344bd9fb3d9bc76f1a4a519726e26c2
Ansar1503 <muhammedansarma1503@gmail.com>
2026-06-25 11:37:05 +0530

    Dashboard header style update

--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -1,12 +1,12 @@
 
 function DashboardHeader() {
   return (
-    <div className="flex justify-between items-center p-10 bg-gray-100 shadow-lg">
+    <div className="flex justify-between items-center p-10 bg-gray-100 shadow">
         <div className="">
             <h1 className="text-4xl font-bold">Konnect Studios</h1>
             <p className="text-lg">Manage and track studio bookings</p>
         </div>
-        <button className="bg-black text-white px-6 py-2 font-semibold cursor-pointer rounded-md hover:bg-gray-500">+ New Booking</button>
+        <button className="bg-black text-white px-6 py-2 font-semibold cursor-pointer rounded hover:bg-gray-500">+ New Booking</button>
     </div>
 
   )
