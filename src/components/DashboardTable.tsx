13c45cfa60dcbaac99f437392dc0a38fd6d268e2
Ansar1503 <muhammedansarma1503@gmail.com>
2026-06-25 11:36:12 +0530

    dashboard table created

---
+++ src/components/DashboardTable.tsx
@@ -0,0 +1,42 @@
+import { StatusBadge } from "@/components/StatusBadge"
+import type { Booking } from "@/types/types";
+import { Trash2 } from "lucide-react"
+
+interface DashboardTableProps {
+    bookings: Booking[];
+}
+
+function DashboardTable({bookings}: DashboardTableProps) {
+    return (
+        <div className="mt-10 mx-8">
+            <table className="w-full text-left border-collapse">
+                <thead className="bg-neutral-300 rounded-xl">
+                    <tr>
+                        <th className="p-3">#</th>
+                        <th className="p-3">Client Name</th>
+                        <th className="p-3">Session Type</th>
+                        <th className="p-3">Date</th>
+                        <th className="p-3">Status</th>
+                        <th className="text-right pr-4">Action</th>
+                    </tr>
+                </thead>
+                <tbody className="bg-neutral-200 text-left">
+                   {bookings.map((booking) => (
+                    <tr key={booking.id}>
+                        <td className="p-2">{booking.id}</td>
+                        <td className="p-2">{booking.clientName}</td>
+                        <td className="p-2">{booking.sessionType}</td>
+                        <td className="p-2">{booking.date}</td>
+                        <td className="p-2"><StatusBadge status={booking.status} /></td>
+                        <td className="text-right pr-4"><button className="p-2 cursor-pointer text-red-600 hover:text-red-400 ">
+                                <Trash2 size={18} />
+                            </button></td>
+                    </tr>
+                   ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default DashboardTable
